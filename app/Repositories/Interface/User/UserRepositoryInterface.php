f50dcfabd61ddde5a134fd8ba3278bd56c6e4681
Donald Blessing <donaldblessing9@gmail.com>
2021-10-29 13:02:57 +0100

    user repository

---
+++ app/Repositories/Interface/User/UserRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Repositories\Interface\User;
+
+use App\Repositories\Interface\BaseRepositoryInterface;
+
+interface UserRepositoryInterface extends BaseRepositoryInterface
+{
+}
